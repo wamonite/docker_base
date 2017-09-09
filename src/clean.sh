c078ee1912f66758b5dd883073fe95e6b9d2f25b
Warren Moore <warren@wamonite.com>
2017-09-09 23:04:04 +0100

    Improvements for testing
    Reordered Dockerfile.
    Single install script.
    Added proper finish scripts.
    Used execline for scripts.
    rsyslog_tail waits for rsyslog to complete.

--- src/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eux
-
-apt-get clean
-rm -rf ~/.gnupg
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-rm -rf /var/log/apt/* /var/log/dpkg.log /var/log/alternatives.log /var/log/bootstrap.log /var/log/dmesg
--- src/install.sh
+++ src/install.sh
@@ -2,31 +2,93 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+BASE_LOCALE=en_GB
+BASE_LANGUAGE=en_GB.UTF-8
+S6_VERSION=1.20.0.0
+DUMBINIT_VERSION=1.2.0
+DUMBINIT_CHECKSUM=81231da1cd074fdc81af62789fead8641ef3f24b6b07366a1c34e5b059faf363
+GOSU_VERSION=1.10
+
+######## PREPARE
+
+# Enable Ubuntu Universe and Multiverse.
+sed -i 's/^#\s*\(deb.*universe\)$/\1/g' /etc/apt/sources.list
+sed -i 's/^#\s*\(deb.*multiverse\)$/\1/g' /etc/apt/sources.list
+apt-get update
+
+# Fix some issues with APT packages (https://github.com/dotcloud/docker/issues/1024)
+dpkg-divert --local --rename --add /sbin/initctl
+ln -sf /bin/true /sbin/initctl
+
+# Replace the 'ischroot' tool to make it always return true.
+# Prevent initscripts updates from breaking /dev/shm.
+# https://journal.paul.querna.org/articles/2013/10/15/docker-ubuntu-on-rackspace/
+# https://bugs.launchpad.net/launchpad/+bug/974584
+dpkg-divert --local --rename --add /usr/bin/ischroot
+ln -sf /bin/true /usr/bin/ischroot
+
+# Install HTTPS support for APT.
+apt-get install -y --no-install-recommends apt-transport-https ca-certificates
+
+# Install add-apt-repository
+apt-get install -y --no-install-recommends software-properties-common
+
+# Upgrade all packages.
+apt-get dist-upgrade -y --no-install-recommends
+
+# Fix locale.
+apt-get install -y --no-install-recommends language-pack-en
+locale-gen ${BASE_LOCALE}
+update-locale LANG=${BASE_LANGUAGE} LC_CTYPE=${BASE_LANGUAGE}
+
+######## INSTALL
+
+# extra packages
 apt-get install -y --no-install-recommends curl less vim-tiny jq rsync
 
 build_arch=$(dpkg --print-architecture)
 
-s6_overlay_ver=1.20.0.0
+# s6
 s6_overlay_dest=/tmp/s6-overlay-${build_arch}.tar.gz
 s6_overlay_sig=/tmp/s6-overlay.sig
-curl -SLo "${s6_overlay_dest}" https://github.com/just-containers/s6-overlay/releases/download/v${s6_overlay_ver}/s6-overlay-${build_arch}.tar.gz
-curl -SLo "${s6_overlay_sig}" https://github.com/just-containers/s6-overlay/releases/download/v${s6_overlay_ver}/s6-overlay-${build_arch}.tar.gz.sig
+curl -SLo "${s6_overlay_dest}" https://github.com/just-containers/s6-overlay/releases/download/v${S6_VERSION}/s6-overlay-${build_arch}.tar.gz
+curl -SLo "${s6_overlay_sig}" https://github.com/just-containers/s6-overlay/releases/download/v${S6_VERSION}/s6-overlay-${build_arch}.tar.gz.sig
 curl https://keybase.io/justcontainers/key.asc | gpg --import
 gpg --verify "${s6_overlay_sig}" "${s6_overlay_dest}"
 tar -xzvf "${s6_overlay_dest}" -C /
 
-dumbinit_ver=1.2.0
+# dumb-init
 dumbinit_dest=/usr/local/bin/dumb-init
-dumbinit_checksum=81231da1cd074fdc81af62789fead8641ef3f24b6b07366a1c34e5b059faf363
-curl -SLo "${dumbinit_dest}" https://github.com/Yelp/dumb-init/releases/download/v${dumbinit_ver}/dumb-init_${dumbinit_ver}_${build_arch}
-echo "${dumbinit_checksum} ${dumbinit_dest}" | sha256sum -c -
+curl -SLo "${dumbinit_dest}" https://github.com/Yelp/dumb-init/releases/download/v${DUMBINIT_VERSION}/dumb-init_${DUMBINIT_VERSION}_${build_arch}
+echo "${DUMBINIT_CHECKSUM} ${dumbinit_dest}" | sha256sum -c -
 chmod 755 "${dumbinit_dest}"
 
-gosu_ver=1.10
+# gosu
 gosu_dest=/usr/local/bin/gosu
 gosu_sig=/tmp/gosu.sig
-curl -SLo "${gosu_dest}" https://github.com/tianon/gosu/releases/download/${gosu_ver}/gosu-${build_arch}
-curl -SLo "${gosu_sig}" https://github.com/tianon/gosu/releases/download/${gosu_ver}/gosu-${build_arch}.asc
+curl -SLo "${gosu_dest}" https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-${build_arch}
+curl -SLo "${gosu_sig}" https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-${build_arch}.asc
 gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
 gpg --verify "${gosu_sig}" "${gosu_dest}"
 chmod 755 "${gosu_dest}"
+
+######## SERVICES
+
+apt-get install -y --no-install-recommends cron rsyslog logrotate
+
+# remove useless cron entries
+rm -f /etc/cron.daily/apt-compat
+rm -f /etc/cron.daily/dpkg
+rm -f /etc/cron.daily/passwd
+rm -f /etc/cron.weekly/fstrim
+
+# remove unused logrotate
+rm -f /etc/logrotate.d/apt
+rm -f /etc/logrotate.d/dpkg
+
+######## CLEAN
+
+apt-get clean
+rm -rf ~/.gnupg
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+rm -rf /var/log/apt/* /var/log/dpkg.log /var/log/alternatives.log /var/log/bootstrap.log /var/log/dmesg
