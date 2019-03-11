8c30ec613e401531fceaed3228c45f61828a9692
Warren Moore <warren@wamonite.com>
2019-03-11 22:11:01 +0000

    Add 16.04-armhf to get latest image script.

--- get_latest_ubuntu_base_image.sh
+++ get_latest_ubuntu_base_image.sh
@@ -11,3 +11,6 @@ cd repo-info/repos/ubuntu/local
 
 echo "16.04: $(ls xenial-*.md | sed -e 's/\.md//')"
 echo "18.04: $(ls bionic-*.md | sed -e 's/\.md//')"
+
+armhf1604="$(curl -fsSL https://raw.githubusercontent.com/balena-io-library/official-images/master/library/raspberrypi3-ubuntu | grep xenial-run | sed -E 's/^{"tag":"(xenial-run-[^"]*)".*$/\1/')"
+echo "16.04-armhf: ${armhf1604}"
