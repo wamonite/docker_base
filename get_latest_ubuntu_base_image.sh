45148f9ff0b4fc3acc95e19c1795bfe7d360dbe7
Warren Moore <warren@wamonite.com>
2019-08-16 14:56:07 +0100

    Update get_latest_ubuntu_base_image.sh script.

--- get_latest_ubuntu_base_image.sh
+++ get_latest_ubuntu_base_image.sh
@@ -1,16 +1,3 @@
 #!/bin/bash -eu
 
-rm -rf /tmp/get_latest_ubuntu_base_image
-mkdir /tmp/get_latest_ubuntu_base_image
-cd /tmp/get_latest_ubuntu_base_image
-
-# this is a bit slow but can't find an easier to scrape info
-git clone --depth 1 git@github.com:docker-library/repo-info.git
-
-cd repo-info/repos/ubuntu/local
-
-echo "16.04: $(ls xenial-*.md | sed -e 's/\.md//')"
-echo "18.04: $(ls bionic-*.md | sed -e 's/\.md//')"
-
-armhf1604="$(curl -fsSL https://raw.githubusercontent.com/balena-io-library/official-images/master/library/raspberrypi3-ubuntu | grep xenial-run | sed -E 's/^{"tag":"(xenial-run-[^"]*)".*$/\1/')"
-echo "16.04-armhf: ${armhf1604}"
+curl -fsSL https://raw.githubusercontent.com/docker-library/official-images/master/library/ubuntu | grep '^Tags:' | sed -e 's/^Tags: \([^,]*\), \([^,]*\).*/\1: \2/' | grep -e 16.04 -e 18.04
