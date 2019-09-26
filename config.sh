443e63a1f68ad0b36ed1a206eed18ecd09099c4b
Warren Moore <warren@wamonite.com>
2019-09-26 21:49:53 +0100

    Update base versions.

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ export DOCKER_REPO=wamonite
 
 container_name=base
 # https://hub.docker.com/_/ubuntu
-build_args_1604="--build-arg IMAGE_BASE=ubuntu:xenial-20190122"
-build_args_1804="--build-arg IMAGE_BASE=ubuntu:bionic-20190204"
+build_args_1604="--build-arg IMAGE_BASE=ubuntu:xenial-20190904"
+build_args_1804="--build-arg IMAGE_BASE=ubuntu:bionic-20190912.1"
 # https://hub.docker.com/r/balenalib/raspberrypi3-ubuntu/
 build_args_1604pi="--build-arg IMAGE_BASE=balenalib/raspberrypi3-ubuntu:xenial-run-20190127"
