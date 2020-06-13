6a43cebd5ffcfed6e0bd4896ba7347c3a5da76b8
Warren Moore <warren@wamonite.com>
2020-06-13 15:23:00 +0100

    Update xenial and bionic images, and pi notes.

--- config.sh
+++ config.sh
@@ -4,8 +4,10 @@ export DOCKER_REPO=wamonite
 
 container_name=base
 # https://hub.docker.com/_/ubuntu
-build_args_1604="--build-arg IMAGE_BASE=ubuntu:xenial-20190904"
-build_args_1804="--build-arg IMAGE_BASE=ubuntu:bionic-20190912.1"
+build_args_1604="--build-arg IMAGE_BASE=ubuntu:xenial-20200326"
+build_args_1804="--build-arg IMAGE_BASE=ubuntu:bionic-20200403"
 build_args_2004="--build-arg IMAGE_BASE=ubuntu:focal-20200423"
 # https://hub.docker.com/r/balenalib/raspberrypi3-ubuntu/
 build_args_1604pi="--build-arg IMAGE_BASE=balenalib/raspberrypi3-ubuntu:xenial-run-20190127"
+# TODO https://www.balena.io/docs/reference/base-images/base-images-ref/
+# TODO https://hub.docker.com/r/balenalib/raspberrypi3-64-ubuntu
